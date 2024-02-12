201ef97e24b27532a61b0cf8a793699af1f6a6a8
Anshuman Rathore <anshumansinghrathore26@gmail.com>
2024-02-12 16:01:05 +0530

    code pushed

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:new_demo/quiz.dart';
+
+
+void main() {
+  runApp(
+      const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Quiz(),
+    ),
+  );
+
+}
